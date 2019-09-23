3c6c5f410ccb79b0f87058ca4d34bb751256ecc4
dan <dan@localhost.localdomain>
2019-09-23 08:18:09 +0300

    add var male

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var male=[
+    "born on sunday; Kwasi",
+    "born on monday; Kwadwo","born on tuesday; Kwabena","born on wednesday; Kwaku","born on thursday; Yaw","born on friday; Kofi","born on saturday; Kwame"
+]
+
+}
