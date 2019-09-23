ca9f9c832a312271e0e20a4024347b9d98791d98
dan <dan@localhost.localdomain>
2019-09-23 08:28:42 +0300

    add function login

--- js/script.js
+++ js/script.js
@@ -5,4 +5,27 @@ var male=[
 var female=[
     "born on sunday; Akosua","born on monday; dwoa","born on tuesday; Abenaa","born on wednesday; Akua","born on thursday;Yaa","born on friday; Afua","born on saturday;Ama"
 ]
+
+function login(){
+    var name=""
+    var gender= document.forms["myForm"]["gender"].value;
+    var date=document.forms["myForm"]["date"].value;
+    var dateObject=new Date(date);
+    var day=dateObject.getDay();
+    if (gender==="male"){
+        name=male[day];
+    }else if (gender==="female"){
+        name=female[day];
+    }else{
+        alert("invalid option")
+    }
+    document.getElementById("result").innerHTML=name;
+    window.scrollTo(0,document.body.scrollHeight);
+
+        var frm = document.getElementsByName('date')[0];
+        frm.submit(); // Submit
+        frm.reset(dateObject);  // Reset
+        return false; // Prevent page refresh
+
+     return name;
 }
