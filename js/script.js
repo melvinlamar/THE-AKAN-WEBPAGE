72573edaf060fcb6a7ba58e84f955a5d15f9ea09
dan <dan@localhost.localdomain>
2019-09-23 08:23:06 +0300

    add var female

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ var male=[
     "born on sunday; Kwasi",
     "born on monday; Kwadwo","born on tuesday; Kwabena","born on wednesday; Kwaku","born on thursday; Yaw","born on friday; Kofi","born on saturday; Kwame"
 ]
-
+var female=[
+    "born on sunday; Akosua","born on monday; dwoa","born on tuesday; Abenaa","born on wednesday; Akua","born on thursday;Yaa","born on friday; Afua","born on saturday;Ama"
+]
 }
